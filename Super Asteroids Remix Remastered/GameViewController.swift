7ea93e74fe890166ecb1d62b189a9149d02ffc1a
Paulo José <paulojocb@gmail.com>
2019-04-05 08:31:31 -0300

    Initial commit

---
+++ Super Asteroids Remix Remastered/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  Super Asteroids Remix Remastered
+//
+//  Created by Paulo José on 05/04/19.
+//  Copyright © 2019 Paulo José. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
